d776db14c79e9b3755714e3fdc3015c8acc550a6
Missjasminthetb <salamijasmin@gmail.com>
2023-04-17 12:13:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let [seconds, minutes, hours] = [0,0,0]
+// let displayTime = document.getElementById("displayTime")
+// let timer = null
+// let start = document.querySelector("#fa-play")
+
+// function stopwatch (){
+//     seconds++;
+//     if (seconds == 60){
+//         seconds = 0;
+//         minutes++;
+//         if(minutes == 60){
+//             minutes = 0;
+//             hours++;
+//         }
+//     }
+//     displayTime.innerHTML = hours +":"+ minutes +":"+ seconds;
+// }
+
+// function watchStart(){
+//     if(timer!== null){
+//         clearInterval(timer);
+//     }
+//     timer = setInterval(stopwatch,1000);
+// }
+// start.addEventListener("click", watchStart);
